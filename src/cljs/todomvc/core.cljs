d19f42d5737f2850430c846081ac879d18fab07e
Ben Brinckerhoff <ben@bbrinck.com>
2014-11-27 10:03:30 -0800

    Adds instructions

--- src/cljs/todomvc/core.cljs
+++ src/cljs/todomvc/core.cljs
@@ -5,13 +5,17 @@
 (defonce state (atom []))
 
 (defn app []
-  [:section {:id "todoapp"}
-   [:div
-    [:header {:id "header"}
-     [:h1 "todos"]
-     [:input {:id "new-todo"
-              :placeholder "What needs to be done?"}
-      ]]]])
+  [:div
+   [:section {:id "todoapp"}
+    [:div
+     [:header {:id "header"}
+      [:h1 "todos"]
+      [:input {:id "new-todo"
+               :placeholder "What needs to be done?"}
+       ]]]]
+   [:footer {:id "info"}
+    [:p "Double-click to edit a todo"]]
+   ])
 
 (defn main []
   (reagent/render-component [app] (.getElementById js/document "app")))
