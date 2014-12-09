b9a8e399acecf8414260a939514dc166ce7cd4ee
Ben Brinckerhoff <ben@bbrinck.com>
2014-12-08 20:15:24 -0800

    Shortens code

--- src/cljs/todomvc/core.cljs
+++ src/cljs/todomvc/core.cljs
@@ -7,33 +7,32 @@
 (def present? (complement clojure.string/blank?))
 (def enter-key 13)
 
+(defonce actions (chan))
 (defonce edit-buffer (atom ""))
 (defonce state (atom {:editing 0 :todos ["foo", "bar", "baz", "bat"]}))
-(defonce actions (chan))
 
 (defn log [x]
   (.log js/console (clj->js x))
   x)
 
-(defn add-todo [state todo]
-  (reset! edit-buffer "")
-  (-> state
-      (update-in [:todos] #(conj % todo))))
-
-(defn update-todo [m idx value]
-  (assoc-in m [:todos idx] value))
-
 (defn remove-nth [n coll]
   (vec (concat (subvec coll 0 n) (subvec coll (inc n)))))
 
-(defn remove-todo [state idx]
+(defn add-todo! [state todo]
+  (reset! edit-buffer "")
+  (update-in state [:todos] #(conj % todo)))
+
+;;(defn update-todo! [m idx value]
+;;  (assoc-in m [:todos idx] value))
+
+(defn remove-todo! [state idx]
   (update-in state [:todos] #(remove-nth idx %)))
 
-(defn update-state [action]
+(defn update-state! [action]
   (when action
     (case (:action action)
-      :add-todo (swap! state add-todo (:value action))
-      :destroy-todo (swap! state remove-todo (:idx action))
+      :add-todo (swap! state add-todo! (:value action))
+      :destroy-todo (swap! state remove-todo! (:idx action))
       )))
 
 (defn add-todo-action [event]
@@ -42,15 +41,11 @@
                (present? value))
       {:action :add-todo :value value})))
 
-(defn event->action [data event]
-  (case (:action data)
-    :add-todo (add-todo-action event)
-    :destroy-todo data
-    nil))
+(defn destroy-todo-action [idx]
+  {:idx idx :action :destroy-todo})
 
-(defn queue-action [data event]
-  (when-let [action (event->action data event)]
-    (put! actions action)))
+(defn queue-action [action]
+  (when action (put! actions action)))
 
 (defn todo [idx content]
   [:li
@@ -59,7 +54,7 @@
              :type "checkbox"}]
     [:label content]
     [:button {:class "destroy"
-              :onClick (partial queue-action {:idx idx :action :destroy-todo})}]
+              :onClick #(queue-action (destroy-todo-action idx))}]
     ]
    ])
 
@@ -72,7 +67,7 @@
            ;; up with typing, so we edit the state directly instead
            ;; of putting the action on a channel
            :on-change #(reset! edit-buffer (-> % .-target .-value))
-           :on-key-down (partial queue-action {:action :add-todo})
+           :on-key-down #(queue-action (add-todo-action %))
            }
    ]
   )
@@ -100,5 +95,4 @@
   (reagent/render-component [app] (.getElementById js/document "app"))
   (go (while true
          (let [action (<! actions)]
-           (update-state action)
-           ))))
+           (update-state! action)))))
